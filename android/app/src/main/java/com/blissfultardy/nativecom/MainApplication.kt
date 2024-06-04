7357b6da4bdf1e211a3ced9fa14c65961b038e07
BlissfulTardy <okka05525@hotmail.com>
2024-06-04 15:23:44 +0300

    Debugging Commit:
    - Tried bugfix implementations for malfunctioning 'rn-firebase .app()' function call preventing whole app from booting
    - Implemented changes on MainApplication.kt, and both build.gradle instances
    - tried but failed to rebuild gradle with the error 'Deprecated Gradle features were used in this build incompatible with 0.9'
    - Failed overall attempt to get firebase or application boot working

--- android/app/src/main/java/com/blissfultardy/nativecom/MainApplication.kt
+++ android/app/src/main/java/com/blissfultardy/nativecom/MainApplication.kt
@@ -2,8 +2,6 @@ package com.blissfultardy.nativecom
 
 import android.app.Application
 import android.content.res.Configuration
-import androidx.annotation.NonNull
-
 import com.facebook.react.PackageList
 import com.facebook.react.ReactApplication
 import com.facebook.react.ReactNativeHost
@@ -15,54 +13,56 @@ import com.facebook.react.defaults.DefaultReactHost.getDefaultReactHost
 import com.facebook.react.defaults.DefaultReactNativeHost
 import com.facebook.react.flipper.ReactNativeFlipper
 import com.facebook.soloader.SoLoader
-
 import expo.modules.ApplicationLifecycleDispatcher
 import expo.modules.ReactNativeHostWrapper
 
 import io.invertase.firebase.app.ReactNativeFirebaseAppPackage
+import io.invertase.firebase.auth.ReactNativeFirebaseAuthPackage
+import io.invertase.firebase.crashlytics.ReactNativeFirebaseCrashlyticsPackage
 
 class MainApplication : Application(), ReactApplication {
 
-  override val reactNativeHost: ReactNativeHost = ReactNativeHostWrapper(
+    override val reactNativeHost: ReactNativeHost = ReactNativeHostWrapper(
         this,
         object : DefaultReactNativeHost(this) {
-          override fun getPackages(): List<ReactPackage> {
-              return listOf(
-                  MainReactPackage(),
-                  ReactNativeFirebaseAppPackage()
-              )
-          }
+            override fun getPackages(): List<ReactPackage> {
+                val packages = PackageList(this).packages.toMutableList()
+                packages.add(ReactNativeFirebaseAppPackage())
+                packages.add(ReactNativeFirebaseAuthPackage())
+                packages.add(ReactNativeFirebaseCrashlyticsPackage())
+                return packages
+            }
 
-          override fun getJSMainModuleName(): String = ".expo/.virtual-metro-entry"
+            override fun getJSMainModuleName(): String = ".expo/.virtual-metro-entry"
 
-          override fun getUseDeveloperSupport(): Boolean = BuildConfig.DEBUG
+            override fun getUseDeveloperSupport(): Boolean = BuildConfig.DEBUG
 
-          override val isNewArchEnabled: Boolean = BuildConfig.IS_NEW_ARCHITECTURE_ENABLED
-          override val isHermesEnabled: Boolean = BuildConfig.IS_HERMES_ENABLED
-      }
-  )
+            override val isNewArchEnabled: Boolean = BuildConfig.IS_NEW_ARCHITECTURE_ENABLED
+            override val isHermesEnabled: Boolean = BuildConfig.IS_HERMES_ENABLED
+        }
+    )
 
-  override val reactHost: ReactHost
-    get() = getDefaultReactHost(this.applicationContext, reactNativeHost)
+    override val reactHost: ReactHost
+        get() = getDefaultReactHost(this.applicationContext, reactNativeHost)
 
-  override fun onCreate() {
-    super.onCreate()
-    SoLoader.init(this, false)
-    if (!BuildConfig.REACT_NATIVE_UNSTABLE_USE_RUNTIME_SCHEDULER_ALWAYS) {
-      ReactFeatureFlags.unstable_useRuntimeSchedulerAlways = false
+    override fun onCreate() {
+        super.onCreate()
+        SoLoader.init(this, /* native exopackage */ false)
+        if (!BuildConfig.REACT_NATIVE_UNSTABLE_USE_RUNTIME_SCHEDULER_ALWAYS) {
+            ReactFeatureFlags.unstable_useRuntimeSchedulerAlways = false
+        }
+        if (BuildConfig.IS_NEW_ARCHITECTURE_ENABLED) {
+            // If you opted-in for the New Architecture, we load the native entry point for this app.
+            load()
+        }
+        if (BuildConfig.DEBUG) {
+            ReactNativeFlipper.initializeFlipper(this, reactNativeHost.reactInstanceManager)
+        }
+        ApplicationLifecycleDispatcher.onApplicationCreate(this)
     }
-    if (BuildConfig.IS_NEW_ARCHITECTURE_ENABLED) {
-      // If you opted-in for the New Architecture, we load the native entry point for this app.
-      load()
-    }
-    if (BuildConfig.DEBUG) {
-      ReactNativeFlipper.initializeFlipper(this, reactNativeHost.reactInstanceManager)
-    }
-    ApplicationLifecycleDispatcher.onApplicationCreate(this)
-  }
 
-  override fun onConfigurationChanged(newConfig: Configuration) {
-    super.onConfigurationChanged(newConfig)
-    ApplicationLifecycleDispatcher.onConfigurationChanged(this, newConfig)
-  }
+    override fun onConfigurationChanged(newConfig: Configuration) {
+        super.onConfigurationChanged(newConfig)
+        ApplicationLifecycleDispatcher.onConfigurationChanged(this, newConfig)
+    }
 }
